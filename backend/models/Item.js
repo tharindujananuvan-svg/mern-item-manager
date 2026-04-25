ee6a89726177178df9c023edc5cae58ad1ef0009
Tharindu Jananuvan <tharindujananuvan@gmail.com>
2026-04-26 00:45:11 +0530

    first commit

---
+++ backend/models/Item.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const ItemSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  price: { type: Number, required: true }, // ← YOU ADD THIS
+}, { timestamps: true });
+
+module.exports = mongoose.model('Item', ItemSchema);
