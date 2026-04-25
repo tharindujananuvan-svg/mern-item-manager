fc9be9efa9b7d937e3ec36fa7a5e49ecb5428949
Tharindu Jananuvan <tharindujananuvan@gmail.com>
2026-04-26 04:50:55 +0530

    fix model import case

--- backend/routes/items.js
+++ backend/routes/items.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Item = require('../models/item.js');
+const Item = require('../models/Item.js');
 // GET all items
 router.get('/', async (req, res) => {
  try {
